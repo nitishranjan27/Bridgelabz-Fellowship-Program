de79faa765eddc6464ee0aa4899afbb560bbb611
nitishranjan27 <nk47595@gmail.com>
2022-01-01 18:06:18 +0530

    Day4Assignment

---
+++ Snake_and_Ladder_Problem/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using SnakeLadderGame;
+
+namespace Snake_and_Ladder_Problem
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Board size?");
+            int boardsize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number of players?");
+            int players = Convert.ToInt32(Console.ReadLine());
+            Game g = new(boardsize, players);
+            g.Play();
+        }
+    }
+}
