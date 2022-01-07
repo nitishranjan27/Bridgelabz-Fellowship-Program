5929231185a9ef8d622ff65967c978fe472bd310
nitishranjan27 <nk47595@gmail.com>
2022-01-07 14:49:17 +0530

    Day__3,4,8,9,10__Assignments

---
+++ Employees_Wage_Computation_Problem/EmpWage_FinalSolution/IComputeEmpWage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees_Wage_Computation_Problem.EmpWage_FinalSolution
+{
+    public interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+        int getTotalWage(string company);
+    }
+}
---
+++ Employees_Wage_Computation_Problem/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using Employees_Wage_Computation_Problem;
+using Employees_Wage_Computation_Problem.EmpWage_FinalSolution;
+
+//EmployeeAttendance.GetAttendance();
+//EmpWages.empWages();
+//PartTimeEmpWage.partTimeEmpWage();
+//EmpWageUsingSwitch.empWageUsingSwitch();
+//CalculateWagesFor20DaysInMonth.calculateWagesFor20DaysInMonth();
+//CalculateWageTill100HrsOr20IsReached.calculateWageTill100HrsOr20IsReached();
+
+//#region EmpWage
+//EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+//EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+//dMart.computeEmpWage();
+//Console.WriteLine(dMart.toString());
+//reliance.computeEmpWage();
+//Console.WriteLine(reliance.toString());
+//#endregion  EmpWageEnding
+
+//#region EmpWageEmployees
+//EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+//empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+//empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+//empWageBuilder.computeEmpWage();
+//#endregion
+
+#region FinalSolution
+EmpWageBuilder empWageBuild = new EmpWageBuilder();
+empWageBuild.addCompanyEmpWage("DMart", 20, 2, 10);
+empWageBuild.addCompanyEmpWage("Reliance", 10, 4, 20);
+empWageBuild.computeEmpWage();
+#endregion
+
+Console.ReadKey();
