8f5ed383378d0bfdb11853c75cafeacbd0aac036
nitishranjan27 <nk47595@gmail.com>
2022-01-17 20:46:32 +0530

    Day_14_Assignment

---
+++ Snake_and_Ladder_Problem/LadderCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder_Problem
+{
+    class LadderCell : Cell
+    {
+        public int AdvantageCell { get; set; }
+    }
+}
---
+++ Snake_and_Ladder_Problem/SnakeCell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_and_Ladder_Problem
+{
+    class SnakeCell : Cell
+    {
+        public int PenaltyCell { get; set; }
+    }
+}
