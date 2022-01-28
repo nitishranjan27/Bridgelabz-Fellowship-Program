78e2ccc22a76e231af95a06955be34ccb699f260
nitishranjan27 <nk47595@gmail.com>
2022-01-28 18:22:18 +0530

    Day_16_Assignment && Day_17_Assignment

---
+++ Algorithm_Problems/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Algorithm_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //InsertionSort
+            int[] arr = { 12, 11, 13, 5, 6 };
+            Console.WriteLine("Welcome to Insertion sort. Sorted array is: ");
+            InsertionSort ob = new InsertionSort();
+            ob.sort(arr);
+
+            //Bubble Sort
+            int[] arr1 = { 64, 34, 25, 12, 22, 11, 90 };
+            BubbleSort obj1 = new BubbleSort();
+            obj1.bubbleSort(arr1);
+            Console.WriteLine("\nWelcome to Bubble sort. Sorted array is: ");
+            obj1.printArray1(arr1);
+
+            // Merge Sort
+            int[] arr2 = { 12, 11, 13, 5, 6, 7 };
+            Console.WriteLine("\nWelcome to Bubble sort: ");
+            Console.WriteLine("Given Array");
+            MergeSort ob2 = new MergeSort();
+            ob2.printArray2(arr2);
+
+            ob2.sort(arr2, 0, arr2.Length - 1);
+            Console.WriteLine("\nSorted array");
+            ob2.printArray2(arr2);
+
+
+            #region Anagram_Detection_Problem
+
+            string firstString, secondString;
+            //Gets words from user.  
+            Console.WriteLine("Enter first string");
+            firstString = Console.ReadLine();
+            Console.WriteLine("Enter second string");
+            secondString = Console.ReadLine();
+            AnagramDetection anagram = new AnagramDetection();
+            //Check if words are anagram  
+            if (anagram.areAnagram(firstString, secondString) == true)
+            {
+                Console.WriteLine("Both string are anagram string.");
+            }
+            else
+            {
+                Console.WriteLine("Both string are not anagram string.");
+            }
+
+            #endregion
+        }
+    }
+}
